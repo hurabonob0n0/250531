2071a927f7fac6a3fd070d3aa346106f2f71ed57
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-07-28 04:30:40 +0900

    커밋

--- Client/MainApp/private/Camera_Free.cpp
+++ Client/MainApp/private/Camera_Free.cpp
@@ -3,7 +3,6 @@
 #include "GameInstance.h"
 #include "Client_Globals.h"
 #include "Terrain.h"
-#include "Network_Manager.h"
 
 CCamera_Free::CCamera_Free() : CCamera()
 {
@@ -34,11 +33,11 @@ HRESULT CCamera_Free::Initialize(void* pArg)
 
 	m_VIBuffer = (CVIBuffer_Geos*)m_GameInstance->Get_Component("VIBuffer_GeosCom", &BS);
 
-	m_TankTransform = (CTransform*)m_GameInstance->Get_Object_Component("Tank", Network_Manager::GetInstance()->GetMyTankIndex(), "TransformCom");
-	Safe_AddRef(m_TankTransform);
+	//m_TankTransform = (CTransform*)m_GameInstance->Get_Object_Component("Tank", 0, "TransformCom");
+	//Safe_AddRef(m_TankTransform);
 
-	m_Tank = (CTank*)m_GameInstance->GetGameObject("Tank", Network_Manager::GetInstance()->GetMyTankIndex());
-	Safe_AddRef(m_Tank);
+	//m_Tank = (CTank*)m_GameInstance->GetGameObject("Tank", 0);
+	//Safe_AddRef(m_Tank);
 
 
 	m_Distance_TPS = 20.f;
