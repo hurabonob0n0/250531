75df8383af399bd7454281891435d2e5c67046a6
kkth3150 <71112653+kkth3150@users.noreply.github.com>
2025-08-14 15:29:23 +0900

    ads

--- Client/MainApp/private/MainApp.cpp
+++ Client/MainApp/private/MainApp.cpp
@@ -224,7 +224,7 @@ HRESULT CMainApp::Initialize(HINSTANCE g_hInstance)
 							Network_Manager::GetInstance()->MyControlTarget = CONTROL_TANK;
 						}
 
-						if (player.Position == 1|| player.Position ==9) {
+						if (player.Position == 1) {
 							Network_Manager::GetInstance()->MyPosMode = POS_MASTER;
 							CStateMgr::Get_Instance()->Set_GameMode(GM_TPS);
 							Network_Manager::GetInstance()->MyControlTarget = CONTROL_TANK;
